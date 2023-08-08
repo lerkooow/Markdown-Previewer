4b771a037bb43c3c27e24b7a31c80107e1035041
lerkooow <lerkoooow@gmail.com>
2023-08-08 19:23:43 +0700

    Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import MarkdownPreview from './MarkdownPreview.js';
 
 function App() {
+  const initialMarkdown = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+
+### And here's some other cool stuff:
+
+`
+  const [postEditor, setPostEditor] = useState(initialMarkdown);
+  const [editorWrap, setEditorWrap] = useState(true);
+  const [previewWrap, setPreviewWrap] = useState(true);
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className={`editorWrap ${previewWrap ? '' : 'none'}`}>
+          <div className="foolbar">
+            <i className="fa-brands fa-free-code-camp"></i>
+            <span id="editor-previewer">Editor</span>
+            <i onClick={() => setEditorWrap((prevEditor) => !prevEditor)} className={editorWrap ? 'fa fa-arrows-alt' : 'fa fa-compress'}></i>
+          </div>
+          <textarea id="editor" value={postEditor} onChange={e => setPostEditor(e.target.value)} className={`${editorWrap ? '' : 'height'}`}></textarea>
+        </div>
+        <div className={`previewWrap ${editorWrap ? '' : 'none'} ${previewWrap ? '' : 'height'}`}>
+          <div className="foolbar">
+            <i className="fa-brands fa-free-code-camp"></i>
+            <span id="editor-previewer">Previewer</span>
+            <i onClick={() => setPreviewWrap((prevPreview) => !prevPreview)} className={previewWrap ? 'fa fa-arrows-alt' : 'fa fa-compress'}></i>
+          </div>
+          <div id="preview" className={`${previewWrap ? '' : 'height'}`}>
+            <MarkdownPreview markdown={postEditor} />
+          </div>
+        </div>
+      </div>
+    </div >
   );
+
 }
+
+
+
+
 
 export default App;
